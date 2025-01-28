391203ff762acf4d3ca0d4901336f314fd436f44
milanaslinkina <79783057+milanaslinkina@users.noreply.github.com>
2025-01-28 16:46:13 +0500

    уведомления

--- templates/listOfChats/listOfChats.js
+++ templates/listOfChats/listOfChats.js
@@ -8,6 +8,25 @@ document.addEventListener('DOMContentLoaded', function() {
         if (parts.length === 2) return parts.pop().split(';').shift();
     }
 
+    function addChatToList(chatName) {
+        const chatItem = document.createElement('div');
+        chatItem.classList.add('chat-item');
+        chatItem.innerHTML = `
+        <div class="user-icon">${chatName[0].toUpperCase()}</div>
+            <div class="user-info">
+              <span class="username">${chatName}</span>
+               <span class="last-message">Нет сообщений</span>
+            </div>
+         `;
+        chatItem.addEventListener('click', () => {
+            const currentUrl = window.location.origin;
+            console.log('Перенаправление на:', `${currentUrl}/templates/chats/chats.html?name=${chatName}`);
+            window.location.href = `${currentUrl}/templates/chats/chats.html?name=${chatName}`;
+        });
+        chatList.appendChild(chatItem);
+    }
+
+
     fetch('/getChats', {
         method: 'POST',
         headers: {
@@ -18,23 +37,10 @@ document.addEventListener('DOMContentLoaded', function() {
         .then(response => response.json())
         .then(data => {
             data.forEach(chat => {
-                const chatItem = document.createElement('div');
-                chatItem.classList.add('chat-item');
-                chatItem.innerHTML = `
-                <div class="user-icon">${chat.name[0].toUpperCase()}</div>
-                 <div class="user-info">
-                    <span class="username">${chat.name}</span>
-                    <span class="last-message">${chat.lastMessage ? chat.lastMessage : "Нет сообщений"}</span>
-                </div>
-                `;
-                chatItem.addEventListener('click', () => {
-                    window.location.href = `/chatPage?name=${chat.name}`;
-                });
-                chatList.appendChild(chatItem);
+                addChatToList(chat.name);
             });
         })
         .catch(error => {
             console.error('Ошибка во время получения чатов:', error);
         });
-
 });
